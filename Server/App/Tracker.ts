e38623e881ba363570f3728e4e23b061df85d15b
Alexander Farrell <alexander.j.farrell@outlook.com>
2022-04-13 15:24:55 -0700

    Committing work. Fixed Home Buttons to be on new system. Added more CSS for Home Buttons. Added more functionality for tracker.

--- Server/App/Tracker.ts
+++ Server/App/Tracker.ts
@@ -3,6 +3,7 @@ import * as e from "express";
 import {Router} from "express";
 import {ContainsBodyArgs, IsLoggedIn, RenderTemplate} from "../Modules/ServerHelper";
 import {Data} from "../Modules/Database";
+import parseDate from "postgres-date";
 
 export class Tracker implements IApp {
     GetName(): string {
@@ -16,7 +17,25 @@ export class Tracker implements IApp {
         router.get("/", async (req, res) => {
             let types = await this.GetTypes();
             let track = await this.GetEntries(req.session['name'], 30);
-            RenderTemplate(res, "Tracker", "tracker", {});
+
+            let r = [
+
+            ]
+
+            let curr = ""
+            track.forEach(row => {
+                if (curr != row['type']) {
+                    curr = row['type'];
+                    r.push([])
+                }
+
+                // @ts-ignore
+                let date = parseDate(row['at'] as string);
+
+                r[r.length-1] = [row['value'], date.toLocaleString()];
+            })
+
+            RenderTemplate(res, "Tracker", "tracker", {types: types, table: r});
         })
 
         router.post('/', async (req, res) => {
@@ -56,7 +75,7 @@ export class Tracker implements IApp {
             `insert into tracker (value, type_id, account_id) VALUES 
 ($1, 
  (select id from tracker_type where name=$2),
- (select id from account where name=$3))`,
+ (select id from account where username=$3))`,
             value,
             type,
             account_name
@@ -72,6 +91,23 @@ export class Tracker implements IApp {
         });
     }
 
+    async GetEntriesBy(type, account_name, days) {
+        return (await Data.Query(
+            `
+select ty.name as type,
+       t.value as value,
+       t.at as at
+from tracker t
+inner join tracker_type ty on ty.id = t.type_id
+where type_id = (select id from tracker_type tt where tt.name = $1)
+and account_id = (select id from account a where a.username = $2)
+order by t.at
+`,
+            type,
+            account_name
+        )).rows;
+    }
+
     async GetEntries(account_name, days) {
         return (await Data.Query(
             `
@@ -79,7 +115,22 @@ select ty.name as type,
        t.value as value,
        t.at as at
 from tracker t
-inner join tracker_type ty on ty.id = tracker.type_id
+inner join tracker_type ty on ty.id = t.type_id
+where account_id = (select id from account a where a.username = $1)
+order by ty.name, t.at
+`,
+            account_name
+        )).rows;
+    }
+
+    async GetEntriesByType(account_name, days) {
+        return (await Data.Query(
+            `
+select ty.name as type,
+       t.value as value,
+       t.at as at
+from tracker t
+inner join tracker_type ty on ty.id = t.type_id
 `
         )).rows;
     }
--- googleplex.js
+++
@@ -1,8 +0,0 @@
-let s = "1";
-let number_of_zeros = 3003;
-
-for (let i = 0; i < number_of_zeros; i += 3) {
-    s += ",000";
-}
-
-console.log(s);
